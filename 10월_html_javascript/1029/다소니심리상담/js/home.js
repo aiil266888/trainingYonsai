ee5e517f8b9303f5212a7ab3cb0c92174a5c4572
kangsumin <aiil266888@gmail.com>
2024-10-31 17:39:51 +0900

    1031

--- 10월_html_javascript/1029/다소니심리상담/js/home.js
+++ 10월_html_javascript/1029/다소니심리상담/js/home.js
@@ -1,5 +1,39 @@
 $(function(){
-    /*hover 동시에 */
-    
 
+
+    var bx = $('.main_slide').bxSlider({
+        auto:true,
+        controls:true,
+        pager:false,
+        mode:'fade',
+        pause:8000,
+        onSlideBefore:function(){},
+        onSlideAfter:function(oldIndex,newIndex){
+            console.log(newIndex);
+            if(newIndex==0){
+                setTimeout(function(){
+                $(".slide1_text1").removeClass("active");
+                $(".slide1_span").removeClass("active");
+                },0);
+                setTimeout(function(){
+                $(".slide0_round_text").addClass("active");
+                $(".slide0_span").addClass("active");
+                $(".slide0_text").addClass("active");
+                },0);
+            }
+            if(newIndex==1){
+                setTimeout(function(){
+                $(".slide0_text").removeClass("active");
+                $(".slide0_span").removeClass("active");
+                $(".slide0_round_text").removeClass("active");
+                },0);
+                
+                setTimeout(function(){
+                $(".slide1_text1").addClass("active");
+                $(".slide1_span").addClass("active");
+                },0);
+                
+            }
+        }
+    });
 });
